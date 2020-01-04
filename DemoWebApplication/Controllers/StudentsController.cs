701cc0252f2484cca04aa46b928b4f2b28e43574
hardik.patel@promactinfo.com <hardik.patel@promactinfo.com>
2020-01-04 16:47:34 +0530

    Init commit

---
+++ DemoWebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using DemoWebApplication.Services.Students;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DemoWebApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly IStudentsRepository _studentsRepository;
+        public StudentsController(IStudentsRepository studentsRepository)
+        {
+            _studentsRepository = studentsRepository;
+        }
+        public async Task<IActionResult> StudentsList()
+        {
+            var students = await _studentsRepository.GetStudents();
+            return View(students);
+        }
+    }
+}
---
+++ DemoWebApplication/Services/Students/IStudentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using DemoWebApplication.Models.ApplicationClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApplication.Services.Students
+{
+    public interface IStudentsRepository
+    {
+        Task<List<StudentsAc>> GetStudents();
+    }
+}
